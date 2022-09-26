6b7916c62a9f4e02ddec35d5b8c6c179d18c5730
mp <mp.262gr@slmail.me>
2022-09-26 21:34:50 +0100

    wip

--- app/api/main.go
+++ app/api/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+type config struct {
+	port int
+	env string
+}
 
 func main() {
-	fmt.Print("hello world")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "environment", "development", "Environment {development|staging|production}")
+	
+	flag.Parse()
+	fmt.Println(cfg)
+
+
 }
